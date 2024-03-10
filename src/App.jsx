b7cc53d2377ba05fc060e3e2b6c99e632e4d3b29
YanaOrlova93 <orlova93zm@mail.ru>
2024-03-10 21:04:29 +0300

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
-// import * as S from './App.styles.js';
 
-
-
-// import Profile from './pages/profile/index.jsx';
-
-
-
-// export const App = () => {
-
-//   return (
-//     <S.Wrapper>
-//       <S.Container>
-//         <Profile />
-//       </S.Container>
-//     </S.Wrapper>
-//   );
-// };
-
-// export default App;
 
 import * as S from './App.styled'
 import './App.css'
 import AppRoutes from './routes'
-// import database from './Firebase'
+
 
 function App() {
-  // console.log(database)
+ 
   return (
     <S.MainApp>
       <AppRoutes />
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Profile from './pages/profile/index'
 
-// import Acquaint from './pages/acquaint/acquaint'
-// import AuthPage from './pages/Authpage/AuthPage'
-// import ProtectedRoute from './components/ProtectedRoute/protected-route'
-// import Main from './pages/Main/Main'
-// import TrainingPage from './pages/training/training'
-
 export default function AppRoutes() {
   return (
     
