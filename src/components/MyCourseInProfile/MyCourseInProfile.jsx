2b4da7bbc1d225d1a3c71e572d7cfba436d84f7b
YanaOrlova93 <orlova93zm@mail.ru>
2024-03-12 16:06:03 +0300

    мои курсы

---
+++ src/components/MyCourseInProfile/MyCourseInProfile.jsx
@@ -0,0 +1,13 @@
+import * as S from './MyCourseInProfile.styles'
+
+function MyCourseInProfile() {
+    return (
+        <S.HeaderStyleMyProfile>
+          <S.NameCourseUser>Мои Курсы</S.NameCourseUser>
+
+          
+    </S.HeaderStyleMyProfile>
+  )
+}
+
+export default MyCourseInProfile
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,12 +1,13 @@
 import * as S from './profile.styled'
 import MyProfile from '../../components/MyProfile/MyProfile'
+import MyCourseInProfile from '../../components/MyCourseInProfile/MyCourseInProfile'
 
 export default function Profile() {
     return (
       <S.StyledSection>
         {/* <Header /> */}
         <MyProfile />
-        {/* <MyCourseInProfile /> */}
+        <MyCourseInProfile />
       </S.StyledSection>
     )
   }
