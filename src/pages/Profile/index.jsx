2c58208ae334be6a2b69bee804a2eb1517405685
YanaOrlova93 <orlova93zm@mail.ru>
2024-03-12 12:38:56 +0300

    profile myprofile correctlogin correctpassword

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,12 @@
+import * as S from './profile.styled'
+import MyProfile from '../../components/MyProfile/MyProfile'
+
+export default function Profile() {
+    return (
+      <S.StyledSection>
+        {/* <Header /> */}
+        <MyProfile />
+        {/* <MyCourseInProfile /> */}
+      </S.StyledSection>
+    )
+  }
