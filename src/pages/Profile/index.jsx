94f53614d3ce03c8ffce8f2a594232dc8d38442a
YanaOrlova93 <orlova93zm@mail.ru>
2024-04-01 20:06:58 +0300

    шапка

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -2,6 +2,7 @@ import * as S from './profile.styled'
 import MyProfile from '../../components/MyProfile/MyProfile'
 import MyCourseInProfile from '../../components/MyCourseInProfile/MyCourseInProfile'
 import { useAuth } from 'hooks/use-auth';
+import Header from 'components/header/header';
 
 export default function Profile() {
    
@@ -9,11 +10,11 @@ export default function Profile() {
 
     return (
       <S.StyledSection>
-        <S.LogoBox to="/">
+        {/* <S.LogoBox to="/">
             <S.LogoChoose src="/img/logo.svg" alt="logo" />
             </S.LogoBox>
-        <S.User>{email}</S.User>
-        {/* <Header /> */}
+        <S.User>{email}</S.User> */}
+        <Header />
         <MyProfile />
         <MyCourseInProfile />
       </S.StyledSection>
