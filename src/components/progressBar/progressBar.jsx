b6576ea336044d5311eb4b0177699774e0d298cf
YanaOrlova93 <orlova93zm@mail.ru>
2024-04-07 17:53:46 +0300

    5 пунктик

--- src/components/progressBar/progressBar.jsx
+++ src/components/progressBar/progressBar.jsx
@@ -1,40 +1,41 @@
 import * as S from './styles.js';
 
+const palette = [
+  {
+    color: "#565eef",
+    background: "rgba(86, 94, 239, 0.1)",
+    id: 0
+  }, 
+  {
+    color: "#FF6D00",
+    background: "rgba(255, 109, 0, 0.1)",
+    id: 1
+  },
+  {
+    color: "#9A48F1",
+    background: "rgba(154, 72, 241, 0.1)",
+    id: 2
+  },
+  {
+    color: "#565eef",
+    background: "rgba(86, 94, 239, 0.1)",
+    id: 3
+  }, 
+  {
+    color: "#FF6D00",
+    background: "rgba(255, 109, 0, 0.1)",
+    id: 4
+  },
+  {
+    color: "#9A48F1",
+    background: "rgba(154, 72, 241, 0.1)",
+    id: 5
+  },
+]
+
+
+
 export default function ProgressBar({ percentage, keyColor}) {
-
-  const palette = [
-    {
-      color: "#565eef",
-      background: "rgba(86, 94, 239, 0.1)",
-      id: 0
-    }, 
-    {
-      color: "#FF6D00",
-      background: "rgba(255, 109, 0, 0.1)",
-      id: 1
-    },
-    {
-      color: "#9A48F1",
-      background: "rgba(154, 72, 241, 0.1)",
-      id: 2
-    },
-    {
-      color: "#565eef",
-      background: "rgba(86, 94, 239, 0.1)",
-      id: 3
-    }, 
-    {
-      color: "#FF6D00",
-      background: "rgba(255, 109, 0, 0.1)",
-      id: 4
-    },
-    {
-      color: "#9A48F1",
-      background: "rgba(154, 72, 241, 0.1)",
-      id: 5
-    },
-  ]
-
   const paletteItem = palette.find(elem => elem.id === keyColor)
   
   return (
